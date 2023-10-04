d07c951ad78cb42c67bb935ac6ed1823978c72eb
Ali Alhasan <ali.alhassan8181@gmail.com>
2023-10-04 15:27:44 +0200

    räknar rätta värden

--- app/src/main/java/com/example/furiouscriminalskalkylator/MainActivity.java
+++ app/src/main/java/com/example/furiouscriminalskalkylator/MainActivity.java
@@ -42,10 +42,21 @@ public class MainActivity extends AppCompatActivity
 
         resultField = findViewById(R.id.textView);
         calculate_Btn = findViewById(R.id.btn_uträkning);
+
+
         calculate_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calculate("dummy ");
+
+                String dummy;
+                dummy = String.valueOf(dropdown.getText());
+
+                if (dummy.equals("Cirklens Area") || dummy.equals("%")){
+                        räknaUt(dummy);
+                }
+                else {
+                    calculate(dummy);
+                }
             }
         }) ;
     }
@@ -54,6 +65,9 @@ public class MainActivity extends AppCompatActivity
     */
     public  void calculate( String operation )
     {
+        input1 =Double.parseDouble(editText1.getText().toString());
+      //  String värde2 = editText2.getText().toString();
+        input2 = Double.parseDouble(editText2.getText().toString());
         switch (operation)
         {
             case "+":
@@ -68,13 +82,13 @@ public class MainActivity extends AppCompatActivity
             case "/":
                 result = input1 / input2 ;
                 break;
-            case "squareRoot":
+            case "√":
                 result = Math.sqrt(input1);
                 break;
             case "%":
                 result = input1 / 100;
                 break;
-            case "pythagoras":
+            case "Pythagoras sats":
                 result = Math.sqrt(Math.pow(input1, 2) + Math.pow(input2, 2)); // Is this correct? just changed the - to a +
                 // result = Math.sqrt( a * a + b * b )
                 break;
@@ -82,7 +96,7 @@ public class MainActivity extends AppCompatActivity
             case "circleArea":
                 result = Math.PI* Math.pow(input1,2);
                 break;
-            case "cylinderVolym":
+            case "Cylinderns volym":
                 result = Math.PI* Math.pow(input1,2) * input2;
                 break;
             default:
@@ -91,5 +105,22 @@ public class MainActivity extends AppCompatActivity
         }
         // TODO Visa resultat
         resultField.setText("Resultatet blir " + result);
+    }
+
+    public void räknaUt(String operator){
+        input1 =Double.parseDouble(editText1.getText().toString());
+        switch (operator){
+            case "Cirklens Area":
+                result = Math.PI* Math.pow(input1,2);
+                break;
+            case "%":
+                result = input1 / 100;
+                break;
+            default:
+                resultField.setText("Du har angett felaktiga värden, var snäll och försök igen. ");
+                break;
+        }
+        resultField.setText("Resultatet blir " + result);
+
     }
 }
